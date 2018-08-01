db061a9878822e2077abf798e33071320eb49e5d
kyoh.maruo <romio_cinderella@yahoo.co.jp>
2018-08-02 02:10:34 +0900

    first commit

---
+++ emacs/init.el
@@ -0,0 +1,154 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(require 'cask)
+(cask-initialize)
+
+;; https://qiita.com/yn01/items/b8d3dcb5be9078a6e27f
+
+;; 環境を日本語、UTF-8にする
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; タブにスペースを使用する
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; 改行コードを表示する
+;;(setq eol-mnemonic-dos "(CRLF)")
+;;(setq eol-mnemonic-mac "(CR)")
+;;(setq eol-mnemonic-unix "(LF)")
+
+;; 複数ウィンドウを禁止する
+(setq ns-pop-up-frames nil)
+
+;; ウィンドウを透明にする
+;; アクティブウィンドウ／非アクティブウィンドウ（alphaの値で透明度を指定）
+;;(add-to-list 'default-frame-alist '(alpha . (0.85 0.85)))
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+;;(tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; カーソル行をハイライトする
+;;(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; スペース、タブなどを可視化する
+;;(global-whitespace-mode 1)
+
+;; スクロールは１行ごとに
+(setq scroll-conservatively 1)
+
+;; シフト＋矢印で範囲選択
+(setq pc-select-selection-keys-only t)
+;;(pc-selection-mode 1)
+
+;; C-kで行全体を削除する
+(setq kill-whole-line t)
+
+;;; dired設定
+(require 'dired-x)
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; beep音を消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
+
+;; Macのキーバインドを使う
+;;(mac-key-mode 1)
+
+;; Macのoptionをメタキーにする
+;;(setq mac-option-modifier 'meta)
+
+;; 選択範囲に色をつける
+(transient-mark-mode t)
+(set-face-background 'region "DeepSkyBlue") ;選択範囲の色
+
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
+
+; server start for emacs-client
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;; emacs コピー
+(defun copy-from-osx ()
+ (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+ (let ((process-connection-type nil))
+     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+       (process-send-string proc text)
+       (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (package-utils yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; yaml-mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+;;; smooth-scroll
+(require 'smooth-scroll)
+(smooth-scroll-mode t)
+
